f69152d23f2e72d93cb5eb897aa47cdc4ccdfd75
André Alves <92445365+Alves-Andre@users.noreply.github.com>
2024-08-12 10:15:38 -0300

    API Restfull usando SpringBoot

---
+++ src/main/java/com/example/basespring/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.basespring.repositories;
+
+import com.example.basespring.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository <ProductModel, UUID>{
+}
